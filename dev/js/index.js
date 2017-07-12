b14d7fbee447bf3ba21c7fc18031453a86943f72
Nikhil Arora <narora200@gmail.com>
2017-07-12 13:22:23 +0530

    Modifying the existent state (Not using immutability)

--- dev/js/index.js
+++ dev/js/index.js
@@ -2,36 +2,41 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
 
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 
-const reducer = (state, action) => {
 
+const userReducer = (state = {}, action) => {
 	switch(action.type){
-		case 'INC':
-			return state + action.payload;
-			break;
-		case 'DEC':
-			return state - action.payload;
-			break;
-		default:
-			return state;		
-	}
-};
 
-const store = createStore(reducer, 0);
+
+		case 'CHANGE_USERNAME': {
+			state.username = action.payload;
+			break;
+		}
+		case 'CHANGE_PASS': {
+			state.pass = action.payload;
+			break;
+		}
+	}
+
+	return state;
+}
+
+const tweetReducer = (state=[], action) => {
+	return state;
+}
+
+const reducers = combineReducers({
+	user: userReducer,
+	tweet: tweetReducer
+});
+
+const store = createStore(reducers);
 
 store.subscribe(() => {
 	console.log("Store changed ", store.getState());
 
 })
 
-store.dispatch({type: 'INC', payload: 1});
-store.dispatch({type: 'INC', payload: 2});
-store.dispatch({type: 'INC', payload: 3});
-store.dispatch({type: 'INC', payload: 4});
-store.dispatch({type: 'INC', payload: 5});
-store.dispatch({type: 'DEC', payload: 5});
-store.dispatch({type: 'DEC', payload: 4});
-store.dispatch({type: 'DEC', payload: 3});
-store.dispatch({type: 'DEC', payload: 2});
-store.dispatch({type: 'DEC', payload: 1});
+store.dispatch({type: 'CHANGE_USERNAME', payload: 'iamoperand'});
+store.dispatch({type: 'CHANGE_PASS', payload: 'pass123'});
