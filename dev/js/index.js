0a0a436df7f9134015899106ef0be5ba126c3083
Nikhil Arora <narora200@gmail.com>
2017-07-12 11:08:44 +0530

    Everything in one file: js/index.js

--- dev/js/index.js
+++ dev/js/index.js
@@ -2,12 +2,36 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
 
-class App extends React.Component{
+import { createStore } from 'redux';
 
-	render(){
-		return (<div>Hello World</div>);
+const reducer = (state, action) => {
+
+	switch(action.type){
+		case 'INC':
+			return state + action.payload;
+			break;
+		case 'DEC':
+			return state - action.payload;
+			break;
+		default:
+			return state;		
 	}
-}
+};
 
+const store = createStore(reducer, 0);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+store.subscribe(() => {
+	console.log("Store changed ", store.getState());
+
+})
+
+store.dispatch({type: 'INC', payload: 1});
+store.dispatch({type: 'INC', payload: 2});
+store.dispatch({type: 'INC', payload: 3});
+store.dispatch({type: 'INC', payload: 4});
+store.dispatch({type: 'INC', payload: 5});
+store.dispatch({type: 'DEC', payload: 5});
+store.dispatch({type: 'DEC', payload: 4});
+store.dispatch({type: 'DEC', payload: 3});
+store.dispatch({type: 'DEC', payload: 2});
+store.dispatch({type: 'DEC', payload: 1});
